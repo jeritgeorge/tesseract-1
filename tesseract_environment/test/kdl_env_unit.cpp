747359d49a181b94f3c01b9620315c77a4a9fa9a
Levi Armstrong <levi.armstrong@gmail.com>
2019-02-25 20:10:40 -0600

    Create tesseract_environment and semi-isolate

--- tesseract_environment/test/kdl_env_unit.cpp
+++ tesseract_environment/test/kdl_env_unit.cpp
@@ -1,17 +1,18 @@
-#include <tesseract_core/macros.h>
-TESSERACT_IGNORE_WARNINGS_PUSH
+#include <tesseract_environment/core/macros.h>
+TESSERACT_ENVIRONMENT_IGNORE_WARNINGS_PUSH
 #include <ros/package.h>
 #include <gtest/gtest.h>
 #include <ros/ros.h>
 #include <fstream>
 #include <urdf_parser/urdf_parser.h>
-#include <srdfdom/model.h>
 #include <algorithm>
-TESSERACT_IGNORE_WARNINGS_POP
+TESSERACT_ENVIRONMENT_IGNORE_WARNINGS_POP
 
-#include "tesseract_ros/kdl/kdl_env.h"
+#include "tesseract_environment/kdl/kdl_env.h"
+#include "tesseract_collision/bullet/bullet_discrete_bvh_manager.h"
+#include "tesseract_collision/bullet/bullet_cast_bvh_manager.h"
 
-void runTest(const tesseract::tesseract_ros::ROSBasicEnvPtr& env)
+void runTest(const tesseract_environment::EnvironmentPtr& env)
 {
   // Test after clone if active list correct
   tesseract_collision::DiscreteContactManagerPtr discrete_manager = env->getDiscreteContactManager();
@@ -26,25 +27,19 @@ void runTest(const tesseract::tesseract_ros::ROSBasicEnvPtr& env)
 
 TEST(TesseractROSUnit, KDLEnvActiveCollisionObjectUnit)
 {
-  std::string urdf_path = ros::package::getPath("tesseract_ros") + "/test/urdf/lbr_iiwa_14_r820.urdf";
+  std::string urdf_path = std::string(DATA_DIR) + "/urdf/lbr_iiwa_14_r820.urdf";
   std::ifstream urdf_ifs(urdf_path);
   std::string urdf_xml_string((std::istreambuf_iterator<char>(urdf_ifs)), (std::istreambuf_iterator<char>()));
 
   urdf::ModelInterfaceSharedPtr urdf_model = urdf::parseURDF(urdf_xml_string);
   assert(urdf_model != nullptr);
 
-  std::string srdf_path = ros::package::getPath("tesseract_ros") + "/test/urdf/lbr_iiwa_14_r820.srdf";
-  std::ifstream srdf_ifs(srdf_path);
-  std::string srdf_xml_string((std::istreambuf_iterator<char>(srdf_ifs)), (std::istreambuf_iterator<char>()));
-
-  srdf::ModelSharedPtr srdf_model = srdf::ModelSharedPtr(new srdf::Model);
-  srdf_model->initString(*urdf_model, srdf_xml_string);
-  assert(srdf_model != nullptr);
-
-  tesseract::tesseract_ros::KDLEnvPtr env(new tesseract::tesseract_ros::KDLEnv);
+  tesseract_environment::KDLEnvPtr env(new tesseract_environment::KDLEnv);
   assert(env != nullptr);
 
-  bool success = env->init(urdf_model, srdf_model);
+  bool success = env->init(urdf_model);
+  env->setDiscreteContactManager(tesseract_collision::tesseract_collision_bullet::BulletDiscreteBVHManagerPtr(new tesseract_collision::tesseract_collision_bullet::BulletDiscreteBVHManager()));
+  env->setContinuousContactManager(tesseract_collision::tesseract_collision_bullet::BulletCastBVHManagerPtr(new tesseract_collision::tesseract_collision_bullet::BulletCastBVHManager()));
   assert(success);
 
   runTest(env);
